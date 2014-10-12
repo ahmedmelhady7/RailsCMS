ff988d36c8b51cc46ef5091538b41cee50da7a14
Ahmed El Hadi <elhadiahmed3@gmail.com>
2014-10-12 14:31:12 -0400

    finished chapter #5 MVC and dynamic data

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,22 @@
 class DemoController < ApplicationController
+
   layout false
   def index
+    #render(:template => "demo/hello")
   end
+
+  def hello
+    #render("index")
+    @name = "Ahmed El Hadi"
+    @page = params['page']
+  end
+
+  def other_hello
+    redirect_to(:controller=>"demo", :action=>"index")
+  end
+  
+  def lynda
+    redirect_to("http://lynda.com")
+  end
+  
 end
